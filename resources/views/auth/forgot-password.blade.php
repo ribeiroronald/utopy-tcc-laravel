9a4ae0076d9a7f5bf5ad4757fd959c2c63c0840f
Etec <gustavo.silva1667@etec.sp.gov.br>
2023-04-12 20:21:58 -0300

    add Jetstream wich Livewire

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -26,7 +26,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 <x-button>
-                    {{ __('Enviar') }}
+                    {{ __('Email Password Reset Link') }}
                 </x-button>
             </div>
         </form>
